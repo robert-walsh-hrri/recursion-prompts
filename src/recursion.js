61247264d098521d101f79fd668156d62a8bad8a
robert-walsh-hrri <rwalsh@gmail.com>
2020-06-10 15:07:39 -0500

    Sum Integers in Array completed

--- src/recursion.js
+++ src/recursion.js
@@ -35,19 +35,15 @@ var sum = function(array) {
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {    
-    // make a shallow copy
-    var workingArray = array.slice();
-    var sum = 0;
-    // recur over the array
-    if (!Array.isArray(workingArray)) {
-        return sum += arraySum(workingArray);
-    }
-    // if there's an inner array iterate over that
-    for (var i = 0; i < workingArray.length; i++) {
-      sum += arraySum(workingArray[i]);
-    }
-    // read the values as integers and return them
-    return sum;
+  if (array.length === 0) {
+      return 0;
+  }
+  return array.reduce((previous, current) => {
+      if (Array.isArray(current)) {
+          return previous + arraySum(current);
+      }
+      return previous + current;
+  }, 0);
 };
 
 // 4. Check if a number is even.
